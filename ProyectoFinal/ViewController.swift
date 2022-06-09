52abd8d71864d994b15f3f3cc0f6df7e90bd4506
Awtry <jorge_verah@outlook.es>
2022-06-09 00:47:47 -0500

    Agregado FireBase
    Solo funciona cuando escribes un correo valido

--- ProyectoFinal/ViewController.swift
+++ ProyectoFinal/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtCorreo: UITextField!
+    @IBOutlet weak var txtContra: UITextField!
+    
+    @IBAction func btnGuardarUsueario(_ sender: UIButton) {
+        
+        Auth.auth().createUser(withEmail: txtCorreo.text!, password: txtContra.text!) { authResult, error in
+        }
+        
+        print("Lo que trae Usuario:", self.txtCorreo.text!)
+        print("Lo que trae cobtra:", self.txtContra.text!)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
